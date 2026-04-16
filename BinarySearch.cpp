18ff809d824c2d1a9394803e7691bb56677cc4a3
muhammadkhalidhidayatullah29-code <muhammadkhalidhidayatullah29@gmail.com>
2026-04-16 11:17:08 +0700

    menambahkan looping do-while, looping do-while dalam looping do-while, struktur kendali if dalam looping do-while dalam looping do-while, dan struktur if dalam looping do-while

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -24,7 +24,7 @@ void input()
     
     cout << "\n==============================================\n";
     cout << "           Masukkan Elemen Array               \n";
-    cout << "===============================================\n";
+    cout << "==============================================\n";
 
     for (int i = 0; i < nPanjang; i++) 
     {
@@ -70,4 +70,45 @@ void display()
 
 void binarySearch() 
 {
-    
+    char ulang;
+    do 
+    {
+        cout << "\n==============================================\n";
+        cout << "          Pencarian Binary Search              \n";
+        cout << "==============================================\n";
+
+        cout << "Masukkan elemen yang ingin dicari : ";
+        cin >> x;
+
+        int low = 0;
+        int high = nPanjang - 1;
+
+        do
+        {
+            int mid = (low + high) / 2;
+
+            if (element[mid] == x) 
+            {
+                cout << "\n[✓] Elemen " << x << " ditemukan pada indeks " << mid << ".\n";
+                return;
+            }
+            if (x < element[mid]) 
+            {
+                high = mid - 1;
+            } 
+            if (x > element[mid]) 
+            {
+                low = mid + 1;
+            }
+        } while (low <= high);
+
+        if (low > high) 
+        {
+            cout << "\n[x] Elemen " << x << " tidak ditemukan dalam array.\n";
+        }
+
+        cout << "\nIngin mencari lagi? (y/n) : ";
+        cin >> ulang;
+
+    } while (ulang == 'y' || ulang == 'Y');
+}
